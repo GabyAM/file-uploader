2f634f521573dcc754f81a9f4285d55e70d95482
GabyAM <gdgabyam@gmail.com>
2024-07-27 12:25:20 -0300

    sets ejs as view engine
    and sets a basic view file to test

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const logger = require('morgan');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use(logger('dev'));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.statusCode || 500).send(err.message);
