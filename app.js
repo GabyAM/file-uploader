cfc5c18ee923c8ac91be98b1d411ba0be3f3b999
GabyAM <gdgabyam@gmail.com>
2024-08-12 10:23:35 -0300

    Final app configurations
    serves the public folder and adds middleware for security and performance.

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const path = require('path');
 const {PrismaSessionStore} = require('@quixo3/prisma-session-store');
 const session = require('express-session');
 const prisma = require('./config/prisma');
+const helmet = require('helmet');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use(logger('dev'));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -29,6 +33,14 @@ app.use(
   })
 );
 
+app.use(helmet());
+app.use(compression());
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000,
+    max: 100,
+  })
+);
 const indexRouter = require('./routes/index');
 const folderRouter = require('./routes/folder');
 const fileRouter = require('./routes/file');
